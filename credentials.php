0f8d23d34905bcdba72a4328a4a2ddf697b977e8
tristan <thao112@umn.edu>
2026-03-17 02:43:23 +0000

    initial commit

--- credentials.php
+++ credentials.php
@@ -1,6 +1,9 @@
 <?php
+
+//Remember to populate the data here
+//
 $servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
-$dbname = 'employees';              //which db you're going to use
-$username = 'phpuser1';
-$password = 'abc123';
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
 ?>
